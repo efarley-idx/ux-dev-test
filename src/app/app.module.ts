55a4759cf5a532d24e65981153d99222c9795e12
Eric Farley <eric.farley@idexpertscorp.com>
2018-02-15 11:18:27 -0800

    Updates 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { RouterModule, RouterLink } from '@angular/router';
@@ -17,6 +18,7 @@ import { WelcomeModule } from './modules/welcome/welcome.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     RouterModule.forRoot(routes),
     WelcomeModule
